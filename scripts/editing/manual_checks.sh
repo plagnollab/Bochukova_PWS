9b369bfd8bf782bc01c8dfa4cc8ff89a2adc827b
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-10-13 18:45:03 +0100

    See CHANGELOGS

--- scripts/editing/manual_checks.sh
+++ scripts/editing/manual_checks.sh
@@ -4,4 +4,8 @@ find /scratch2/vyp-scratch2/Bochukova_RNASeq/processed/set3 -name *unique.bam > 
 
 #samtools mpileup -r 1:29529699-29529699 -f /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta -b list_BAMs.tab > chr1_29529699.tab
 
-samtools mpileup -r 2:25042749-25042749 -f /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta -b list_BAMs.tab > chr2_25042749.tab
+#samtools mpileup -r 2:25042749-25042749 -f /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta -b list_BAMs.tab > chr2_25042749.tab
+
+#samtools mpileup -r 4:158257875-158257875 -f /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta -b list_BAMs.tab > chr4_158257875.tab
+
+samtools mpileup -r 2:160626593-160626593 -f /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta -b list_BAMs.tab > chr2_160626593.tab

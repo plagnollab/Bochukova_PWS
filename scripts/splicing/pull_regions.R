139930860c9ecbfbdf5a365491034960e0a28005
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-11-17 10:35:18 +0000

    See CHANGELOGS

--- scripts/splicing/pull_regions.R
+++ scripts/splicing/pull_regions.R
@@ -6,15 +6,24 @@ library(GenomicRanges)
 fa = FaFile("/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fa")
 
 data <- read.csv("/scratch2/vyp-scratch2/Bochukova_RNASeq/processed/set3_hg38/dexseq/control_PW/Bochukova_set3_control_PW_SignificantExons.csv")
-
+data <- data[ !is.na(data$strand), ]
 
 nseqs <- 300
 
 my.grange <- GenomicRanges::GRanges(data$chromosome[1:nseqs], IRanges(data$exon.start[1:nseqs]-40, data$exon.end[1:nseqs]+40))
 
-my.grange.reduced <- my.grange[  data$exon.end[1:nseqs] - data$exon.start[1:nseqs] < 500 ]
 
-seq = getSeq(fa, my.grange.reduced)
-names(seq) <- paste0(names(seq), "_", 1:length(seq))
+seq <- getSeq(fa, my.grange)
 
-writeFasta(seq, file = "exons_spliced.fa")
+seq.orientated <- ifelse (data$strand[ 1:nseqs ] == 1, as.character(seq), as.character(Biostrings::reverseComplement(seq)))
+seq.orientated <- DNAStringSet(seq.orientated)
+
+seq.orientated <- seq.orientated[ width(seq.orientated) < 500 ]
+names(seq.orientated) <- paste0(names(seq.orientated), "_", 1:length(seq.orientated))
+
+writeFasta(seq.orientated, file = "exons_spliced.fa")
+
+
+print(table(as.character(subseq(seq, start = 39, end = 40))))
+print(table(as.character(subseq(seq.orientated, start = 39, end = 40))))
+print(table(as.character(subseq(seq.orientated, start = width(seq.orientated) - 39, end = width(seq.orientated)-38))))

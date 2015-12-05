0dfca72d8c5cbbc9fe4314336b7777cae92b1ea7
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-12-05 00:58:00 +0000

    See CHANGELOGS

--- scripts/heatmap.R
+++ scripts/heatmap.R
@@ -1,44 +1,30 @@
 library(pheatmap)
-library(DESeq2)
 
 
-load("set3_hg38/deseq2/control_PW/deseq2_object.RData")
+diff.expression.pvalues <- read.table("set3_hg38/deseq2/control_PW/deseq_Bochukova_set3_differential_expression.tab", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+diff.expression.pvalues <- diff.expression.pvalues[ !is.na(diff.expression.pvalues$external_gene_id), ]
 
 
-annotations <- read.table("/scratch2/vyp-scratch2/reference_datasets/RNASeq/Human_hg38/biomart_annotations_human.tab", header = TRUE, stringsAsFactors = FALSE)
+good.genes <- diff.expression.pvalues$external_gene_id[ 1:100 ]
 
 
+RPKM.values <- read.csv("/scratch2/vyp-scratch2/Bochukova_RNASeq/processed/set3_hg38/deseq2/rpkm_values.csv", stringsAsFactors = FALSE)
+RPKM.values <- RPKM.values[ RPKM.values$external_gene_id %in% good.genes, ]
 
+RPKM.mat <- RPKM.values[, grepl(pattern = "Control|PWS", names(RPKM.values)) ]
+RPKM.mat <- log2(as.matrix(RPKM.mat) + 1)
+dimnames(RPKM.mat)[[1]] <- RPKM.values$external_gene_id
 
-my.results <- results(CDS)
-
-pdf("figs/MAplot.pdf")
-plotMA(my.results, main="MA-plot", ylim=c(-4,4))
-dev.off()
-
-
-
-select <-   order(my.results$pvalue)[1:100]
-nt <- rlog(CDS) # defaults to log2(x+1)
-
-
-pdf("figs/PCA.pdf")
-plotPCA(nt, intgroup=c("condition"))
-dev.off()
-
-
-log2.norm.counts <- assay(nt)[select,]
-
-clean.names <- annotations$external_gene_name[ match(rownames(log2.norm.counts), annotations$EnsemblID)  ]
-good.names <- !is.na(clean.names)
-log2.norm.counts <- log2.norm.counts[ good.names, ]
-rownames(log2.norm.counts) <- clean.names [ good.names ]
-
-df <- as.data.frame(colData(CDS)[,c("condition")])
-names(df) <- "Condition"
-
+annot.col<- data.frame(Condition = c("Control", "Control", "Control", "Control", "PWS", "PWS", "PWS", "PWS"))
+row.names(annot.col) <- dimnames(RPKM.mat)[[2]]
 
 pdf("figs/heatmap.pdf", width = 8, height = 8, onefile = FALSE)
-pheatmap(log2.norm.counts, cluster_rows=TRUE, show_rownames=TRUE,
-         cluster_cols=TRUE, annotation_col=df, fontsize_row = 4)
+pheatmap(mat = RPKM.mat,
+         cluster_rows=TRUE,
+         show_rownames=TRUE,
+         cluster_cols=TRUE,
+         annotation_col=annot.col,
+         fontsize_row = 4)
 dev.off()
+
+

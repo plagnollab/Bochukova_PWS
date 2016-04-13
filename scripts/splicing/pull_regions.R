7a33ee10f4eba4df3c3799216be933604a774e43
Vincent Plagnol <vincent.plagnol@gmail.com>
2016-04-13 23:15:18 +0100

    See CHANGELOGS

--- scripts/splicing/pull_regions.R
+++ scripts/splicing/pull_regions.R
@@ -3,21 +3,32 @@ library(ShortRead)
 library(GenomicRanges)
 
 
-fa = FaFile("/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fa")
+fa = FaFile("/cluster/scratch3/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fa")
 
-#data <- read.csv("/scratch2/vyp-scratch2/Bochukova_RNASeq/processed/set3_hg38/dexseq/control_PW/Bochukova_set3_control_PW_SignificantExons.csv")
-#data <- data[ !is.na(data$strand), ]
+data <- read.csv("/cluster/scratch3/vyp-scratch2/Bochukova_RNASeq/processed/set3_hg38/dexseq/control_PW/Bochukova_set3_control_PW_SignificantExons.csv", stringsAsFactors = FALSE)
+data <- data[ !is.na(data$strand), ]
+
+
+tab.n.exons.5pc <- table(subset(data, FDR < 0.1)$external_gene_id)
+tab.n.exons.1pc <- subset(data, FDR < 0.01)$external_gene_id
+
+good.genes <- names(which(tab.n.exons == 1))
+v.good.genes <- intersect( good.genes, tab.n.exons.1pc)
+
+
+sig.data <- data[ data$external_gene_id %in% v.good.genes & !is.na(data$external_gene_id) & (data$FDR < 0.01) & !is.na(data$FDR), ]
+
 
 nseqs <- 280
 my.width <- 220
-my.grange <- GenomicRanges::GRanges(data$chromosome[1:nseqs], IRanges(data$exon.start[1:nseqs]- my.width, data$exon.end[1:nseqs]+my.width))
+my.grange <- GenomicRanges::GRanges(sig.data$chromosome[1:nseqs], IRanges(sig.data$exon.start[1:nseqs]- my.width, sig.data$exon.end[1:nseqs]+my.width))
 
 
 seq <- getSeq(fa, my.grange)
 
-seq.orientated <- ifelse (data$strand[ 1:nseqs ] == 1, as.character(seq), as.character(Biostrings::reverseComplement(seq)))
+seq.orientated <- ifelse (sig.data$strand[ 1:nseqs ] == 1, as.character(seq), as.character(Biostrings::reverseComplement(seq)))
 seq.orientated <- DNAStringSet(seq.orientated)
-names(seq.orientated) <- paste0(data$external_gene_id[ 1:nseqs], "_", 1:length(seq.orientated))
+names(seq.orientated) <- paste0(sig.data$external_gene_id[ 1:nseqs], "_", 1:length(seq.orientated))
 
 seq.orientated.5p <- subseq(seq.orientated, start = 1, end = my.width - 10)
 seq.orientated.3p <- subseq(seq.orientated, start = width(seq.orientated) - my.width + 10, end = width(seq.orientated))

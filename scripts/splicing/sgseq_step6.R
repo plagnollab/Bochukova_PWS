83e08592b51fd9ba5e7bc0b3196f5d52cad8349f
Vincent Plagnol <vincent.plagnol@gmail.com>
2017-01-14 10:08:43 +0000

    See changelogs

--- scripts/splicing/sgseq_step6.R
+++ scripts/splicing/sgseq_step6.R
@@ -23,5 +23,17 @@ res.clean <- res.clean[ !is.na(res.clean$log2fold_PW_control), ]
 res.clean$FDR <- p.adjust(res.clean$pvalue, method = 'fdr')
 
 
-res.clean <- res.clean[order(res.clean$pvalue), ] 
-write.table(res.clean, file = "sgseq_res.csv", sep = ",") 
+res.clean <- res.clean[order(res.clean$pvalue), ]
+res.clean$geneName <- sapply(res.clean$geneName, paste, collapse = "_")
+
+res.clean$external_geneName <- annotation$external_gene_name[ match(res.clean$geneName, table = annotation$EnsemblID) ]
+
+res.clean$variantType.clean <- sapply(res.clean$variantType, paste, collapse = "_")
+
+annotation <- read.table("/cluster/project8/vyp/vincent/Software/RNASeq_pipeline/bundle/human_hg38/biomart/biomart_annotations_human.tab", stringsAsFactors = FALSE, header = TRUE)
+
+write.csv(res.clean, file = "sgseq_res.csv",  row.names = FALSE) 
+
+
+countEvents <- sort(table( dplyr::filter(res.clean, FDR < 0.1)$variantType.clean))
+
--- scripts/splicing/test_SNORD_targets.R
+++ scripts/splicing/test_SNORD_targets.R
@@ -1,13 +1,23 @@
 library(DESeq2)
-load("set3_hg38/dexseq/control_PW/dexseq_Bochukova_set3_control_PW.RData")
 
+targets <- read.csv("data/SNORD116_targets.csv", stringsAsFactors = FALSE)
+exons.sig.v2 <- read.csv("sgseq_res.csv", stringsAsFactors = FALSE)
+
+
+all.genes <- data.frame(gene = unique(exons.sig.v2$external_geneName))
+all.genes$signif <- all.genes$gene %in% exons.sig.v2$external_geneName[ exons.sig.v2$FDR < 0.1 ]
+all.genes$SNO.target <- all.genes$gene %in% targets$Gene.name
+fisher.test(table(all.genes$signif, all.genes$SNO.target))
+
+
+
+stop()
+load("set3_hg38/dexseq/control_PW/dexseq_Bochukova_set3_control_PW.RData")
 exons.sig <- read.csv("set3_hg38/dexseq/control_PW/Bochukova_set3_control_PW_SignificantExons.csv")
 exons.sig <- exons.sig[ ! grepl(pattern = "^RP|^AC|^AL", exons.sig$external_gene_id), ]
 exons.sig <- exons.sig[ !is.na(exons.sig$external_gene_id), ]
 exons.sig <- exons.sig[ exons.sig$meanBase > 1, ]
 
-
-targets <- read.csv("data/SNORD116_targets.csv", stringsAsFactors = FALSE)
 
 
 all.genes <- data.frame(gene = unique(exons.sig$external_gene_id))
